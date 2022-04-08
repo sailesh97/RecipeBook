39b1dc0e00534a1d2c5d66a594423b9bdaaca979
Sailesh Patra <saileshpatra997@gmail.com>
2022-04-08 23:49:52 +0530

    Preload data from firebase only it's loaded for the 1st time

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { DataStorageService } from "../shared/data-storage.service";
 import { Recipe } from "./recipe.model";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]>{
 
-    constructor(private dataStorageService: DataStorageService){}
+    constructor(private recipeService: RecipeService, private dataStorageService: DataStorageService){}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-        return this.dataStorageService.fetchRecipes();
+        const receipes = this.recipeService.getRecipes();
+        if(receipes.length == 0){
+            return this.dataStorageService.fetchRecipes();
+        } else{
+            return receipes;
+        }
     }
 }
