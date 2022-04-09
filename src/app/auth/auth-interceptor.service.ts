5e3fb4e8469e7b76e0ec79fd01e0dfb3e6349ff3
Sailesh Patra <saileshpatra997@gmail.com>
2022-04-09 14:49:16 +0530

    Attaching token with an Interceptor

---
+++ src/app/auth/auth-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { exhaustMap, take } from "rxjs/operators";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor{
+    constructor(private authService: AuthService){}
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return this.authService.user.pipe(take(1), exhaustMap(
+            user => {
+                console.log("User----------", user)
+                if(!user){
+                    return next.handle(req);
+                }
+                const modifiedRequest = req.clone({
+                    params: new HttpParams().set('auth', user.token)
+                });
+                return next.handle(modifiedRequest); 
+            }            
+        ));
+    }
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -19,12 +19,7 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
-                const token = user.token;
-                return this.http.get<Recipe[]>('https://receipe-book-3f084-default-rtdb.firebaseio.com/recipes.json',{
-                    params: new HttpParams().set('auth', token)
-                })
-            }),
+        return this.http.get<Recipe[]>('https://receipe-book-3f084-default-rtdb.firebaseio.com/recipes.json').pipe(
             map(recipes => {
                 return recipes.map(recipe => {
                     return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
@@ -32,8 +27,25 @@ export class DataStorageService {
             }),
             tap(recipes => {
                 this.recipeService.setRecipes(recipes);
-            })
-        )
-           
+            }))
     }
+
+    // fetchRecipes() {
+    //     return this.authService.user.pipe(take(1), exhaustMap(user => {
+    //             const token = user.token;
+    //             return this.http.get<Recipe[]>('https://receipe-book-3f084-default-rtdb.firebaseio.com/recipes.json',{
+    //                 params: new HttpParams().set('auth', token)
+    //             })
+    //         }),
+    //         map(recipes => {
+    //             return recipes.map(recipe => {
+    //                 return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+    //             });
+    //         }),
+    //         tap(recipes => {
+    //             this.recipeService.setRecipes(recipes);
+    //         })
+    //     )
+
+    // }
 }
