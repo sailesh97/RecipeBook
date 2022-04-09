265b40a219221f5e77885ad1abbd89d19142f83f
Sailesh Patra <saileshpatra997@gmail.com>
2022-04-09 20:01:10 +0530

    Adding AuthGuard to recipes route

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { map, take, tap } from "rxjs/operators";
+import { AuthService } from "./auth.service";
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate{
+    constructor(private router: Router, private authService: AuthService){}
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.authService.user.pipe(take(1), map((user) => {
+            const isAuth = !!user;
+            if(isAuth){
+                return true;
+            }
+            return this.router.createUrlTree(['/auth']);
+        }))
+        // return this.authService.user.pipe(map((user) => {
+        //     return !!user;
+        // }), tap(isAuth => {
+        //     if(!isAuth){
+        //         this.router.navigate(['/auth']);
+        //     }
+        // }))
+    }
+}
