5332c61dc445baec01037cea2b6672a5b33fad03
Sailesh Patra <saileshpatra997@gmail.com>
2022-04-14 14:27:17 +0530

    Lazy-loading implemented

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
