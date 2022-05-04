683a0f04da99133676d6f2e9e43659bfe4ab5e3a
Sailesh Patra <saileshpatra997@gmail.com>
2022-05-04 18:49:47 +0530

    16. First Summary & Clean up

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import * as fromShoppingList from './store/shopping-list.reducer';
 import * as ShoppingListActions from './store/shopping-list.actions';
 @Component({
@@ -15,7 +14,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ ingredients: Ingredient[]}>;
   private igChangeSub: Subscription;
 
-  constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) { } 
+  constructor(private store: Store<fromShoppingList.AppState>) { } 
 
   ngOnInit(): void {
     this.ingredients = this.store.select('shoppingList');
